493ef0a52ad2207d998fd2f3d75e802bf80f8349
jiya yadav <jiyayadav102@gmail.com>
2024-03-15 22:36:25 +0545

    Mother's dashboard created

---
+++ users/learn/connect/foot.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer" style="background: #ccc;">
+        <div class="my-auto">
+          <div class="text-center my-auto">
+          <span style="color: #000;">Copyright &copy;Post Partum Pal <script>document.write(new Date().getFullYear());</script> ~ Developed By : </span><span class="text-primary"> Jiya Yadav</span>
+          </div>
+        </div>
+</footer>
---
+++ users/learn/videolink.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+include "session_st.php";
+include"../../connect.php";
+
+if (!isset($_SESSION['passi'])){
+  header("location:../../login.php");
+  exit();
+}?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <link rel="icon" href="img/faviconn.png">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>EMP E-learn - Video Link Tutorial</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../../w3.css">
+  <link rel="stylesheet" type="text/css" href="../../styles/bootstrap4/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include "connect/head.php"; ?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <?php include "connect/nav.php"; ?>
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Video Link Tutorial</h1>
+            
+          </div>
+          <div class="row">
+            <div class="col-xl-12 col-lg-5">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Video Link Tutorials</h6>
+                  <div class="dropdown no-arrow">
+                   
+                   
+                  </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+				<div class="row">
+
+<!-- SUBJECT -->
+<?php include "function/subjc.php"; ?>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <?php include "connect/foot.php"; ?>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+</body>
+</html>
