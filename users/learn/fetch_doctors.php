83223b6e9b55540ccd98eb55fdb204632f64f2bd
jiya yadav <jiyayadav102@gmail.com>
2024-05-16 11:38:40 +0545

    Added fetch_doctors page to get doctors data from database

---
+++ users/learn/fetch_doctors.php
@@ -0,0 +1,27 @@
+<?php
+include"../../connect.php";
+
+if (isset($_POST['address'])) {
+    $address = mysqli_real_escape_string($con, $_POST['address']);
+    $query = "";
+
+    if ($address == 'near_me') {
+        // Fetch doctors near the user's address
+        // Replace 'user_address' with the actual user's address variable
+        $user_address = $_SESSION['addr'];// This should be dynamically fetched based on the logged-in user
+        $query = "SELECT fullname, address FROM ad_in WHERE address='$user_address'";
+    } else {
+        // Fetch doctors based on the selected address
+        $query = "SELECT fullname, address FROM ad_in WHERE address='$address'";
+    }
+
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
+    $doctors = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $doctors[] = $row;
+    }
+
+    echo json_encode($doctors);
+}
+?>
